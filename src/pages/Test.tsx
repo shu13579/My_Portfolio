46c0496a710ebea33e19fe9489d337ad7ddcf309
Shuto Urabe <urabe_s25@yahoo.co.jp>
2024-02-22 01:33:46 +0900

    feat: 日記機能の修正

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-import '../styles/TestPage.css';
+import React, { useState, useEffect } from 'react';
+import '../styles/Test.css';  // CSSファイルのインポート
+import diaryEntriesData from '../assets/diaryEntries.json';
 
 interface DiaryEntry {
   date: string;
@@ -9,41 +10,19 @@ interface DiaryEntry {
 
 const Test: React.FC = () => {
   const [diaryEntries, setDiaryEntries] = useState<DiaryEntry[]>([]);
-  const [newEntry, setNewEntry] = useState<DiaryEntry>({ date: '', title: '', content: '' });
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    setDiaryEntries([...diaryEntries, newEntry]);
-    setNewEntry({ date: '', title: '', content: '' });
-  };
+  useEffect(() => {
+    setDiaryEntries(diaryEntriesData);
+  }, []);
 
   return (
-    <div>
+    <div className="container">  {/* CSSクラス名を直接使用 */}
       <h1>ここはテストページです。</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="date"
-          value={newEntry.date}
-          onChange={(e) => setNewEntry({ ...newEntry, date: e.target.value })}
-        />
-        <input
-          type="text"
-          value={newEntry.title}
-          placeholder="タイトル"
-          onChange={(e) => setNewEntry({ ...newEntry, title: e.target.value })}
-        />
-        <textarea
-          value={newEntry.content}
-          placeholder="日記の内容"
-          onChange={(e) => setNewEntry({ ...newEntry, content: e.target.value })}
-        />
-        <button type="submit">追加</button>
-      </form>
-      <section>
+      <section className="section">
         {diaryEntries.map((entry, index) => (
-          <div key={index}>
-            <h2>{entry.title} ({entry.date})</h2>
-            <p>{entry.content}</p>
+          <div key={index} className="sectionDiv">
+            <h2 className="sectionH2">{entry.title} ({entry.date})</h2>
+            <p className="sectionP">{entry.content}</p>
           </div>
         ))}
       </section>
