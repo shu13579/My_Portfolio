b757f44d01167d08243dfa2c661684af846e80eb
Shuto Urabe <urabe_s25@yahoo.co.jp>
2024-03-04 00:27:22 +0900

    add: aboutページの内容を追加

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import '../styles/About.css';
 
 const About: React.FC = () => {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>This is the About page.</p>
+    <div className="about">
+      <h1>About Me</h1>
+      <p>こんにちは、私の名前は卜部柊人です。東京工業大学情報理工学院情報工学系の4年生です。</p>
+
+      <h2>現在の焦点</h2>
+      <p>現在は、エンジニアインターンでのWebアプリケーションの開発や個人開発によるコーディングスキルの向上、基本情報技術者の資格取得に向けた学習、英語学習に注力しています。</p>
+
+      <p>このサイトでは、私の経験やプロジェクトを共有したいと思っています。ご興味があれば、ぜひ私のスキルセットページもご覧ください。</p>
     </div>
   );
 };
